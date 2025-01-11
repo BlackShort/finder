62f71203b10a334d289f6fa1cda9acef537140ad
Priyanshu <Priyanshukumarofficials@gmail.com>
2025-01-12 03:30:35 +0530

    Resolving form isssue

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Metadata } from 'next'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Finder",
+  title: "FinderX",
   description: "Find courses and resources to help you learn and grow",
 };
 
